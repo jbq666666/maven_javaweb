a89e959b0c648ae97f2d128539f76196ee951a58
win <953718802@qq.com>
2019-05-15 22:48:03 +0800

    增加User实体类和Main 测试方法

---
+++ src/main/java/com/dolen/bean/Main.java
@@ -0,0 +1,8 @@
+package com.dolen.bean;
+
+public class Main {
+    public static void main(String[] args) {
+        User u =new User("zs",1);
+        System.out.println(u.getName()+"************"+u.getAge());
+    }
+}
